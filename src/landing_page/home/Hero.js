daeb5b553c6e08163a9d2e33f82dcf6a43123e9a
ArmanSingh7 <businessapp7781@gmail.com>
2025-08-02 01:02:12 +0530

    Awards and Hero.js updated with images and basic works

--- src/landing_page/home/Hero.js
+++ src/landing_page/home/Hero.js
@@ -1,14 +1,14 @@
 import React from 'react';
 function Hero() {
     return ( 
-        <div className='container p-5'>
-            <div className='row'>
+        <div className='container p-5 mb-5'>
+            <div className='row text-center'>
                 <img src='media/homeHero.png' alt='Hero_image' className='mb-5'/>
             
             <h1 className='mt-5'>Invest in everything  </h1>
             <p>Online platform to invest in stocks,
                 derivatives, mutual funds, and more</p>
-            <button>Sign Up Now</button>
+            <button className='p-2 btn btn-primary fs-5 mb-5' style={{width:"20%",margin:"0 auto",borderRadius:'10px'}}>Sign Up Now</button>
             </div>
         </div>
      );
