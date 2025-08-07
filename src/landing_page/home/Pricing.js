071aedc1693e63490a9b605f70f3e75b313b2ea1
ArmanSingh7 <businessapp7781@gmail.com>
2025-08-07 23:56:14 +0530

    Pricing updated

--- src/landing_page/home/Pricing.js
+++ src/landing_page/home/Pricing.js
@@ -1,7 +1,19 @@
 import React from 'react';
 function Pricing() {
     return ( 
-        <h1>Pricing</h1>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-4'></div>
+                <h1 className='mb-3'>Unbeatable pricing</h1>
+                <p>We pioneered the concept of discount broking
+                    and price transparency in India. Flat fees and no hidden charges.
+                </p>
+                 <a href='' className='mx-5' style={{textDecoration:"none"}}>Try Kite demo <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                <div className='col-2'></div>
+                <div className='col-6'></div>
+
+            </div>
+        </div>
      );
 }
 
