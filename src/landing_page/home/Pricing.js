785ab27ab0c1b9c010ffbb33d2399e5f6c045b59
ArmanSingh7 <businessapp7781@gmail.com>
2025-08-10 23:14:56 +0530

    Pricing and Stats completed

--- src/landing_page/home/Pricing.js
+++ src/landing_page/home/Pricing.js
@@ -1,16 +1,27 @@
 import React from 'react';
 function Pricing() {
     return ( 
-        <div className='container'>
-            <div className='row'>
-                <div className='col-4'></div>
-                <h1 className='mb-3'>Unbeatable pricing</h1>
-                <p>We pioneered the concept of discount broking
-                    and price transparency in India. Flat fees and no hidden charges.
+        <div className='container p-5'>
+            <div className='row  p-5'>
+                <div className='col-4'>
+                <h1 className='mb-3 fs-3'>Unbeatable pricing</h1>
+                <p>We pioneered the concept of discount broking and price transparency in India. Flat fees and no hidden charges.
                 </p>
-                 <a href='' className='mx-5' style={{textDecoration:"none"}}>See Price <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                 <a href='' className='mx-0 fs-5' style={{textDecoration:"none"}}>See Pricing <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+                 </div>
                 <div className='col-2'></div>
-                <div className='col-6'></div>
+                <div className='col-6'>
+                    <div className='row text-center'>
+                        <div className='col p-5 border'>
+                            <h1 className='mb-3'>₹0</h1>
+                            <p>Free equity delivery and <br></br>direct mutual funds</p>
+                        </div>
+                        <div className='col p-5 border'>
+                            <h1 className='mb-3'>₹20</h1>
+                            <p>Intra Day & F/O</p>
+                        </div>
+                    </div>
+                </div>
 
             </div>
         </div>
