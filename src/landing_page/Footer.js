b54d4069950936886375eabe25970e1130d930b4
ArmanSingh7 <businessapp7781@gmail.com>
2025-08-14 23:24:39 +0530

    Footer half

--- src/landing_page/Footer.js
+++ src/landing_page/Footer.js
@@ -1,7 +1,63 @@
 import React from 'react';
 function Footer() {
     return ( 
-        <h1>Footer</h1>
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <img src="media/logo.svg" style={{width:"50%"}}/>
+                    <p>© 2010 - 2025, Zerodha Broking Ltd.
+                        All rights reserved.</p>
+                </div>
+                <div className="col">
+                    <p>Account</p>
+                     <a href="">Open demat account</a><br/>
+                    <a href="">Minor demat account</a><br/>
+                    <a href="">NRI demat account</a><br/>
+                    <a href="">Commodity</a><br/>
+                    <a href="">Dematerialisation</a><br/>
+                    <a href="">Fund transfer</a><br/>
+                    <a href="">MTF</a><br/>
+                    <a href="">Referral program</a><br/>
+                </div>
+                <div className="col">
+                    <p>Support</p>
+                    
+                   <a href="">Contact us</a><br/>
+                   <a href="">Support portal</a><br/>
+                   <a href="">How to file a complaint?</a><br/>
+                   <a href="">Status of your complaints</a><br/>
+                   <a href="">Bulletin</a><br/>
+                   <a href="">Circular</a><br/>
+                   <a href="">Z-Connect blog</a><br/>
+                   <a href="">Downloads</a><br/>
+                </div>
+                <div className="col">
+                    <p>Company</p>
+                    <a href="">About</a><br/>
+                    <a href="">Philosophy</a><br/>
+                    <a href="">Press & media</a><br/>
+                    <a href="">Careers</a><br/>
+                    <a href="">Zerodha Cares (CSR)</a><br/>
+                    <a href="">Zerodha.tech</a><br/>
+                    <a href="">Open source</a><br/>
+                    
+                </div>
+                <div className="col">
+                    <p>Quick Links</p>
+                    
+                    <a href="">Upcoming IPOs</a><br/>
+                    <a href="">Brokerage charges</a><br/>
+                    <a href="">Market holidays</a><br/>
+                    <a href="">Economic calendar</a><br/>
+                    <a href="">Calculators</a><br/>
+                    <a href="">Markets</a><br/>
+                    <a href="">Sectors</a><br/>
+                    
+                </div>
+                
+                
+            </div>
+        </div>
      );
 }
 
