83a781ffd424e9700277caff183d48f1df01d27f
ArmanSingh7 <businessapp7781@gmail.com>
2025-08-30 23:04:36 +0530

    ABOUT 1

--- src/landing_page/about/Hero.js
+++ src/landing_page/about/Hero.js
@@ -10,7 +10,7 @@ function Hero() {
                 Now, we are breaking ground with our technology.
             </h1>
         </div>
-        <div className='row p-5 fs-6' style={{color:"#424242"}}>
+        <div className='row fs-6 mt-5' style={{color:"#424242",lineHeight:"1.8",fontWeight:"400", paddingLeft:"10%",paddingRight:"10%"}}>
             <div className='col p-5'>
                 <p >We kick-started operations on the 15th of August, 2010 with the goal of breaking all barriers that traders and investors face in India in terms of cost, support, and technology. We named the company Zerodha, a combination of Zero and "Rodha", the Sanskrit word for barrier.
                </p>
@@ -27,7 +27,7 @@ function Hero() {
             <div className='col-6 p-5'>
                 
                 <p>In addition, we run a number of popular open online educational and community initiatives to empower retail traders and investors.</p>
-                <p> <a href=" ">Rainmatter</a>, our fintech fund and incubator, has invested in several fintech startups with the goal of growing the Indian capital markets.</p>
+                <p> <a href=" " style={{textDecoration:"none"}}>Rainmatter</a>, our fintech fund and incubator, has invested in several fintech startups with the goal of growing the Indian capital markets.</p>
                 
                 <p>And yet, we are always up to something new every day. Catch up on the latest updates on our blog or see what the media is saying about us or learn more about our business and product philosophies.</p>
                 
