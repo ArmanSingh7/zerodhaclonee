b9f027c2f11a942b042142e9ca496a5458f2e397
ArmanSingh7 <businessapp7781@gmail.com>
2025-09-09 21:16:35 +0530

    Add dashboard as normal folder

---
+++ dashboard/src/component/Fund.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Fund = () => {
+  return (
+    <div>Fund</div>
+  )
+}
---
+++ dashboard/src/component/Holding.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Holding = () => {
+  return (
+    <div>Holding</div>
+  )
+}
