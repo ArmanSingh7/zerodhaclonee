4ce40075d0d850d60220f73535ec8596e3a6cee8
ArmanSingh7 <businessapp7781@gmail.com>
2025-09-10 22:06:33 +0530

    dashboard creating comp finished

--- dashboard/src/component/Dashboard.js
+++ dashboard/src/component/Dashboard.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Route, Routes } from "react-router-dom";
+import Apps from "./Apps";
+import Funds from "./Fund";
+import Holding from "./Holding";
 
-export const Dashboard = () => {
+import { Orders } from './Orders';
+import { Positions } from './Positions';
+import { Summary } from './Summary';
+import { WatchList } from './WatchList';
+export const Home = () => {
   return (
     <div>Dashboard</div>
   )
